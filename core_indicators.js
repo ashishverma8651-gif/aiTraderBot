7786c6a490cb6aee88ea8d01e58956c5d87553a3
ashishverma8651-gif <ashishverma8651@gmail.com>
2025-11-11 20:17:12 +0530

    Update core_indicators.js

--- core_indicators.js
+++ core_indicators.js
@@ -1,1 +1,10 @@
-//core_indicators.js
+// core_indicators.js
+export function calculateRSI(data) {
+  // Dummy RSI logic
+  return Math.random() * 100;
+}
+
+export function calculateMACD(data) {
+  // Dummy MACD logic
+  return { macd: Math.random(), signal: Math.random() };
+}//core_indicators.js
