efb21d2217026be9d2cab879583aea0890dbfbb2
ashishverma8651-gif <ashishverma8651@gmail.com>
2025-11-11 18:42:40 +0530

    Update utils.js

--- utils.js
+++ utils.js
@@ -5,3 +5,7 @@ export function keepAlive(url) {
     fetch(url).then(() => console.log("💓 Keep-alive ping sent")).catch(()=>{});
   }, 1000 * 60 * 4); // every 4 minutes
 }
+
+export function nowLocal() {
+  return new Date().toLocaleString("en-IN", { timeZone: "Asia/Kolkata" });
+}
