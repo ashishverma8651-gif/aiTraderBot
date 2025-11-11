abae292edc4f42dd8d6f655343547e289fe39dcd
ashishverma8651-gif <ashishverma8651@gmail.com>
2025-11-11 18:40:18 +0530

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,1 +1,7 @@
 //utils.js
+
+export function keepAlive(url) {
+  setInterval(() => {
+    fetch(url).then(() => console.log("💓 Keep-alive ping sent")).catch(()=>{});
+  }, 1000 * 60 * 4); // every 4 minutes
+}
