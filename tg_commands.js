86812ddc7c7b1a50c086f6769177fe68075dc74d
ashishverma8651-gif <ashishverma8651@gmail.com>
2025-11-15 01:39:14 +0530

    Update tg_commands.js

--- tg_commands.js
+++ tg_commands.js
@@ -5,7 +5,7 @@ import { fetchMultiTF } from "./utils.js";
 import * as indicators from "./core_indicators.js";
 import { analyzeElliott } from "./elliott_module.js";
 
-import { runMLPrediction, runMicroPrediction, calculateAccuracy } from "./ml_module_v16.js";
+import { runMLPrediction, runMicroPrediction, calculateAccuracy } from "./ml_module_v8_6.js";
 
 // Telegram init
 const BOT_TOKEN = CONFIG?.TELEGRAM?.BOT_TOKEN || process.env.BOT_TOKEN;
