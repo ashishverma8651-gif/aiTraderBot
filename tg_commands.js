8889205aad6f0e6f073ba416cd9bd76bd7f94a06
ashishverma8651-gif <ashishverma8651@gmail.com>
2025-11-17 23:07:15 +0530

    Update tg_commands.js

--- tg_commands.js
+++ tg_commands.js
@@ -6,7 +6,7 @@
 // Telegram + helpers
 // --------------------
 import TelegramBot from "node-telegram-bot-api";
-import ML from "./ml_module_8_6.js";
+import ML from "./ml_module_v8_6.js";
 import News from "./news_social.js";
 import { analyzeElliott } from "./elliott_module.js";
 import { fetchMultiTF, fetchMarketData } from "./utils.js";
